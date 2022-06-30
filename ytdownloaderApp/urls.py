7b3df4fe5d641a4926f7dd35972d4737fcb57729
godfish <txt4dm@gmail.com>
2022-06-30 12:16:09 +0200

    def

--- ytdownloaderApp/urls.py
+++ ytdownloaderApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-  path('',views.index),
+  path('yt/',views.index),
   path('thanks/<str:id>/<str:format>',views.thx),
   path('error/',views.error),
   path('download/<str:id>/<str:format>', views.download_client)
